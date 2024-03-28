fb152e426481f337f564179629026b5119656b3d
Pierre Rouleau <prouleau001@gmail.com>
2024-03-28 15:52:17 -0400

    Add script to setup USRHOME & template for initial persistent config.
    - setup/template/setfor-zsh-config.zsh is a template to use as
      the usrcfg/setfor-zsh-config.zsh which will hold the user's
      persistent configurations.
      That file is outside of the USRHOME repository and can be
      stored inside another (private) repository managed by the
      user.
    - setup/setup-usrhome is a POSIX compliant sh script that
      installs the files required to use USRHOME.  The script
      describes what would be done and prompts the user before
      proceeding.  It creates the usrcfg directory, put the
      template file inside it and then set's up the Z shell
      configuration files by creating symlinks to the USRHOME
      relevant files.  Any old files are renamed as backup and
      the value of old symlinks printed on stdout before re-linking.

---
+++ setup/template/setfor-zsh-config.zsh
@@ -0,0 +1,39 @@
+# Sourced script: setfor-zsh-config  -*- mode: sh; -*-
+#
+# Purpose   : Template for the Private USRHOME configuration for Z Shell.
+# Created   : Tuesday, March 26 2024.
+# Author    : Pierre Rouleau <prouleau001@gmail.com>
+# Time-stamp: <2024-03-28 10:58:55 EDT, updated by Pierre Rouleau>
+# ----------------------------------------------------------------------------
+# Module Description
+# ------------------
+#
+# This file holds the persistent user settings for the Z Shell controlled by
+# USRHOME.  This file is a *template* provided as an example.
+#
+# To use it with USRHOME you need to store a copy of that file (potentially
+# modified to fit your needs) into a directory named usrcfg.  That directory
+# must be a sibling to the usrhome directory; they must be located inside the
+# same parent directory.
+#
+# The usrhome/setup/setup-usrhome script creates the directory and stores this
+# file if you wish to use it.
+
+# ----------------------------------------------------------------------------
+# Code : modify it to suit your needs.
+# ------------------------------------
+#
+
+# Activation control.  Value 0 deactivate, 1: activates
+# -----------------------------------------------------
+
+# Activate to trace Z Shell configuration file sourced.
+export USRHOME_TRACE_SHELL_CONFIG=0
+
+# Activate when Homebrew is used.
+export USRHOME_USE_HOMEBREW=1
+
+# Activate display of user name and host name on the prompt.
+export USRHOME_PROMPT_SHOW_USR_HOST=1
+
+# ----------------------------------------------------------------------------
