019af7ef9a953c076dfe7f1a01e3eda2960c459c
Pierre Rouleau <prouleau001@gmail.com>
2024-04-06 12:52:07 -0400

    Add sanitize-path POSIX compliant command.
    Can be used inside zsh and Bash shells to cleanup the PATH.

--- dot/zshrc.zsh
+++ dot/zshrc.zsh
@@ -4,7 +4,7 @@
 # Author    : Pierre Rouleau <prouleau001@gmail.com>
 # Copyright (C) 2024 by Pierre Rouleau
 # Created   : Monday, March 18 2024.
-# Time-stamp: <2024-04-06 02:53:17 EDT, updated by Pierre Rouleau>
+# Time-stamp: <2024-04-06 12:05:09 EDT, updated by Pierre Rouleau>
 #
 # ----------------------------------------------------------------------------
 # Module Description
@@ -257,33 +257,7 @@ function usrhome-switch-path {
 # Sanitize PATH
 # -------------
 #
-# - Replace '::' by ':'
-# - Remove duplicate entries in the PATH, leave first one seen.
-# - Then remove the trailing ':' injected.
-#
-# If the old path had to be sanitized, display a warning
-# describing the number of directory entries in each.
-# Use xargs to remove leading spaces from the number strings.
-
-path_entries="$(printf "%s" "$PATH" | tr ':' '\n' | wc -l | xargs)"
-sanitized_path="$(printf "%s" "$PATH" | sed 's/::/:/g' | tr ':' '\n' | awk '!seen[$0]++' | tr '\n' ':')"
-if [[ "${sanitized_path:0-1}" = ":" ]]; then
-    sanitized_path="${sanitized_path: 0:-1}"
-fi
-
-sanitized_path_entries="$(printf "%s" "$sanitized_path" | tr ':' '\n' | wc -l | xargs)"
-if [[ "$path_entries" != "$sanitized_path_entries" ]]; then
-    echo "WARNING: USRHOME has sanitized your PATH!"
-    if [[ "$USRHOME_TRACE_SHELL_CONFIG" = "1" ]]; then
-        echo "         It had $path_entries directories, it now has $sanitized_path_entries."
-        echo " The original PATH was:"
-        showpath -n
-    else
-        echo "Set USRHOME_TRACE_SHELL_CONFIG to 1 in to see more info."
-        echo "- Edit: \$USRHOME_DIR_USRCFG/setfor-zsh-config.zsh"
-    fi
-fi
-export PATH=$sanitized_path
+source $USRHOME_DIR/ibin/do-sanitize-path
 
 # ----------------------------------------------------------------------------
 # Source User Extra zshrc if it exists
