1b2cf1b2a8c9a6eafe563363661e36ca9aed7127
Pierre Rouleau <prouleau001@gmail.com>
2024-04-19 18:21:51 -0400

    Properly handle tracing of the execution of base user zsh config.

--- dot/zshenv.zsh
+++ dot/zshenv.zsh
@@ -4,7 +4,7 @@
 # Author    : Pierre Rouleau <prouleau001@gmail.com>
 # Copyright (C) 2024 by Pierre Rouleau
 # Created   : Monday, March 18 2024.
-# Time-stamp: <2024-04-19 17:45:58 EDT, updated by Pierre Rouleau>
+# Time-stamp: <2024-04-19 18:20:06 EDT, updated by Pierre Rouleau>
 #
 # ----------------------------------------------------------------------------
 # Module Description
@@ -20,6 +20,25 @@
 # ----
 #
 #
+usrhome_trace_in()
+{
+    # Arg 1: string: trace text.  printed after the level.
+    #                Something like:  ~/.zshenv   --> \$USRHOME_DIR/dot/zshenv.zsh
+    #                            or:  \$USRHOME_DIR/ibin/envfor-usrhome
+
+    local title
+    title=$1
+    if [[ "$USRHOME_TRACE_SHELL_CONFIG" = "1" ]]; then
+        if [ -z "$USRHOME_TRACE_LEVEL" ]; then
+            USRHOME_TRACE_LEVEL=1
+            export USRHOME_TRACE_LEVEL
+        else
+            USRHOME_TRACE_LEVEL=$(( USRHOME_TRACE_LEVEL + 1 ))
+        fi
+        printf "-%s-: Sourcing %s\n" "$USRHOME_TRACE_LEVEL"  "$title"
+    fi
+}
+
 # Identify the path of the usrcfg directory by taking advantage that
 # usrhome and usrcfg are llocated inside the same parent, and that
 # this script is executed via a symbolic link.
@@ -41,25 +60,10 @@ else
 fi
 unset usrhome_zsh_config
 
-usrhome_trace_in()
-{
-    # Arg 1: string: trace text.  printed after the level.
-    #                Something like:  ~/.zshenv   --> \$USRHOME_DIR/dot/zshenv.zsh
-    #                            or:  \$USRHOME_DIR/ibin/envfor-usrhome
-
-    local title
-    title=$1
-    if [[ "$USRHOME_TRACE_SHELL_CONFIG" = "1" ]]; then
-        if [ -z "$USRHOME_TRACE_LEVEL" ]; then
-            USRHOME_TRACE_LEVEL=1
-            export USRHOME_TRACE_LEVEL
-        else
-            USRHOME_TRACE_LEVEL=$(( USRHOME_TRACE_LEVEL + 1 ))
-        fi
-        printf "-%s-: Sourcing %s\n" "$USRHOME_TRACE_LEVEL"  "$title"
-    fi
-}
-
+# This script needs to source user configuration scripts to
+# figure out whether tracing is allowed, it sourced other files
+# that reported nested tracing.  The level must be reset to 1.
+unset USRHOME_TRACE_LEVEL
 usrhome_trace_in "~/.zshenv   --> \$USRHOME_DIR/dot/zshenv.zsh"
 
 # cleanup
