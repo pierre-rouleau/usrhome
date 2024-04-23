35b1a209d9be1b787246065b6e5372a8ebe07144
Pierre Rouleau <prouleau001@gmail.com>
2024-04-22 22:31:04 -0400

    Add ability to trace bash_profile on systems that execute  ~/.bash_profile

--- dot/bash_profile.bash
+++ dot/bash_profile.bash
@@ -3,7 +3,7 @@
 # Purpose   : Bash ~/.bash_profile Configuration File - Sourced in interactive login shell.
 # Created   : Sunday, April  7 2024.
 # Author    : Pierre Rouleau <prouleau001@gmail.com>
-# Time-stamp: <2024-04-22 21:21:37 EDT, updated by Pierre Rouleau>
+# Time-stamp: <2024-04-22 22:29:39 EDT, updated by Pierre Rouleau>
 # ----------------------------------------------------------------------------
 # Module Description
 # ------------------
@@ -42,13 +42,9 @@
 # $USRHOME_DIR/dot/bashrc.bash file as would most implementation would do.
 # Then it unset the variable.  This way the other sourced files can detect if
 # they are sourced during login.
-
-# ----------------------------------------------------------------------------
-# Dependencies
-# ------------
 #
-#
-
+# However, before doing it, it executes the logic that allows activation of
+# the USRHOME tracing.
 
 # ----------------------------------------------------------------------------
 # Code
@@ -56,10 +52,63 @@
 #
 #
 
+# 1 - Set USRHOME_DIR and USRHOME_DIR_USRCFG
+# ------------------------------------------
+#
+# Identify the path of the usrcfg directory by taking advantage that usrhome
+# and usrcfg are located inside the same parent directory, and that this
+# script is executed via a symbolic link.
+#
+# If it has already been done, as indicated by the existence of the
+# USRHOME_DIR environment variable, then skip this part.
+#
+if [[ -z $USRHOME_DIR ]]; then
+    # - Identify the name of what bash executed (that should be the ~/.bashrc symlink)
+    script=${BASH_SOURCE[0]}
+    # - Then identify the real file pointed by that symlink: it should be
+    #   this file,the usrhome/dot/bashrc.bash, with the complete path.
+    original_script="$(readlink "$script")"
+    # Then identify the parent directory of the file, that's the parent
+    # of the usrcfg directory too.
+    usrhome_parent="$(dirname "$(dirname "$(dirname "$original_script")")")"
+    export USRHOME_DIR="$usrhome_parent/usrhome"
+    export USRHOME_DIR_USRCFG="$usrhome_parent/usrcfg"
+fi
 
-. $USRHOME_DIR/dot/shell-tracing.sh
+
+# 2 - Determine is shell tracing is activated
+# -------------------------------------------
+#
+usrhome_trace_activation="$USRHOME_DIR_USRCFG/setfor-shell-tracing.sh"
+if [ ! -e "$usrhome_trace_activation" ]; then
+    printf "ERROR: USRHOME cannot find the user's shell tracing configuration file!\n"
+    printf " It is expected at: %s\n" "$usrhome_trace_activation"
+    printf " Please write it, use the template example as basis.\n"
+    printf " The template is: %s/template/usrcfg/setfor-shell-tracing.sh\n" "$USRHOME_DIR"
+else
+    . "$usrhome_trace_activation"
+fi
+unset usrhome_trace_activation
+
+
+# 3 - Define USRHOME shell tracing functions
+# ------------------------------------------
+. "${USRHOME_DIR}/ibin/shell-tracing.sh"
+
+
+# 4 - Trace Shell Configuration if required
+# -----------------------------------------
+#
+# This script needs to source user configuration scripts to
+# figure out whether tracing is allowed, it sourced other files
+# that reported nested tracing.  The level must be reset to 1.
+if [ -n "$USRHOME_TRACE_LEVEL" ]; then
+    unset USRHOME_TRACE_LEVEL
+fi
+# shellcheck disable=SC2088
 usrhome_trace_in "~/.bash_profile   --> \$USRHOME_DIR/dot/bash_profile.bash"
 
+# ----------------------------------------------------------------------------
 # Mimic behaviour found on most Bash implementations:
 # - The ~/.bash_profile sources ~/.bashrc if it exists.
 #   - In the case of USRHOME implementation ~/.bashrc
